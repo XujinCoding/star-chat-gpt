c884cf7c73da9533261e5115c6a4eeff2bf861a8
XuJ <xujin@startimes.com.cn>
2023-03-30 22:14:56 +0800

    初步实现流返回

--- service/openai/openai-service/src/main/java/com/openai/controller/OpenAiController.java
+++ service/openai/openai-service/src/main/java/com/openai/controller/OpenAiController.java
@@ -1,30 +1,60 @@
 package com.openai.controller;
 
 import com.plexpt.chatgpt.ChatGPT;
+import com.plexpt.chatgpt.ChatGPTStream;
+import com.plexpt.chatgpt.entity.chat.Message;
+import com.plexpt.chatgpt.listener.SseStreamListener;
 import com.plexpt.chatgpt.util.Proxys;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.net.Proxy;
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/api/open-ai")
 public class OpenAiController {
-    @RequestMapping(value = "/message",method = RequestMethod.GET)
-    private String getAnswerByMessage(){
-        Proxy proxy = Proxys.http("127.0.0.1", 1080);
+    private static final Proxy proxy = Proxys.http("127.0.0.1", 53859);
+    private static final String key = "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";
 
+    @RequestMapping(value = "/message/{message}",method = RequestMethod.GET)
+    private String getAnswerByMessage(@PathVariable("message") String message){
         ChatGPT chatGPT = ChatGPT.builder()
-                .apiKey("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")
+                .apiKey(key)
                 .proxy(proxy)
                 .apiHost("https://api.openai.com/")
                 .build()
                 .init();
 
-        String res = chatGPT.chat("写一段七言绝句诗，题目是：火锅！");
-        return res;
+        return chatGPT.chat(message);
     }
+
+
+    @GetMapping("/chat/sse")
+    @CrossOrigin
+    public SseEmitter sseEmitter(String prompt) {
+        ChatGPTStream chatGPTStream = ChatGPTStream.builder()
+                .timeout(600)
+                .apiKey(key)
+                .proxy(proxy)
+                .apiHost("https://api.openai.com/")
+                .build()
+                .init();
+
+        SseEmitter sseEmitter = new SseEmitter(-1L);
+
+        SseStreamListener listener = new SseStreamListener(sseEmitter);
+        Message message = Message.of(prompt);
+
+        listener.setOnComplate(msg -> {
+            //回答完成，可以做一些事情
+            System.out.println("回答完成");
+        });
+        chatGPTStream.streamChatCompletion(Arrays.asList(message), listener);
+
+
+        return sseEmitter;
+    }
+
 
 }
